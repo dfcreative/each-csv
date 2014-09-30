49120950f2d4811616495ec1608739d55aa13573
Dmitry <df.creative@gmail.com>
2014-09-30 19:30:58 +0400

    Avoid string checking (faster than requiring mutypes)

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 module.exports = eachCSV;
 
-//match every comma-separated element ignoring 1-level parenthesis, like `1,2(3,4),5`
-var commaMatchRe = /(,[^,]*?(?:\([^()]+\)[^,]*)?)(?=,|$)/g
+/** match every comma-separated element ignoring 1-level parenthesis, e.g. `1,2(3,4),5` */
+var commaMatchRe = /(,[^,]*?(?:\([^()]+\)[^,]*)?)(?=,|$)/g;
 
-//iterate over every item in string
+/** iterate over every item in string */
 function eachCSV(str, fn){
-	if (typeof str !== 'string') return;
+	//force string be primitive
+	str += '';
 
 	var list = (',' + str).match(commaMatchRe) || [''];
 	for (var i = 0; i < list.length; i++) {
